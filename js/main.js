28ce19b203ee96e226ce505f96b74b08d0ddf879
unknown <consolelogreece@gmail.com>
2024-12-26 12:22:54 +0000

    bug fix and save zip with backup now too

--- js/main.js
+++ js/main.js
@@ -4,15 +4,19 @@ const datasizeRgx = /(\nDataSize:=)([0-9]+)\n/;
 const savedatasizeRgx = /(\nSaveDataSize:=)([0-9]+)\n/;
 var saveEditorElement;
 var saveHeader;
+var originalFile;
+var originalFileName;
 var enc = new TextEncoder();
 var dec = new TextDecoder();
 
 document.getElementById('fileInput').addEventListener('change', function(event) {
     const file = event.target.files[0];
+    originalFileName = event.target.files[0].name
     if (file) {
       const reader = new FileReader();
       reader.onload = function(e) {
         const fileContent = e.target.result;
+        originalFile = fileContent
         go(arrayBufferToString(fileContent));
       };
 
@@ -33,7 +37,17 @@ function saveFile()
   saveHeader = saveHeader.replace(savedatasizeRgx, "\nSaveDataSize:=" + compressedXml.byteLength + "\n")
   var headerBlob = new Blob([saveHeader], { type: 'text/plain' });
   var binaryBlob = new Blob([compressedXml], { type: "application/octet-stream" })
-  saveAs(new Blob([headerBlob, binaryBlob]), "save.xml");
+
+  var zip = new JSZip();
+  
+  zip.file(originalFileName, new Blob([headerBlob, binaryBlob]))
+  zip.file("backup of " + originalFileName, new Blob([originalFile]))
+
+  zip.generateAsync({ type: "blob" }).then(function(content) {
+    saveAs(content, "save.zip");
+  });
+
+  //saveAs(new Blob([headerBlob, binaryBlob]), "save.xml");
 }
 
 function go(filecontent){
