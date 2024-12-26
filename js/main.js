0bf5c163a48fa3b309ec8163701e8336c3bada30
unknown <consolelogreece@gmail.com>
2024-12-26 19:19:31 +0000

    stuff

--- js/main.js
+++ js/main.js
@@ -1,83 +1,35 @@
-
-const headerRgx = /^((?:(?:XLZF|[A-Za-z]+:=.*?)\n)+)/;
-const datasizeRgx = /(\nDataSize:=)([0-9]+)\n/;
-const savedatasizeRgx = /(\nSaveDataSize:=)([0-9]+)\n/;
 var saveEditorElement;
-var saveHeader;
 var originalFile;
-var originalFileName;
-var enc = new TextEncoder();
-var dec = new TextDecoder();
-
 document.getElementById('fileInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    originalFileName = event.target.files[0].name
-    if (file) {
+    originalFile = event.target.files[0];
+    if (originalFile) {
       const reader = new FileReader();
       reader.onload = function(e) {
-        const fileContent = e.target.result;
-        originalFile = fileContent
-        go(arrayBufferToString(fileContent));
+        renderSave(arrayBufferToString(e.target.result));
       };
 
-      reader.readAsArrayBuffer(file)
+      reader.readAsArrayBuffer(originalFile)
     }
   });
 
   document.getElementById('saveButton').addEventListener('click', function(event) {
-    saveFile()
+    saveFile(saveEditorElement.get(), originalFile)
   });
 
-function saveFile()
-{
-  var saveJson = saveEditorElement.get()
-  var saveXml = json2xml(saveJson)
-  saveHeader = saveHeader.replace(datasizeRgx, "\nDataSize:=" + saveXml.length + "\n")
-  var compressedXml = compress(enc.encode(saveXml))
-  saveHeader = saveHeader.replace(savedatasizeRgx, "\nSaveDataSize:=" + compressedXml.byteLength + "\n")
-  var headerBlob = new Blob([saveHeader], { type: 'text/plain' });
-  var binaryBlob = new Blob([compressedXml], { type: "application/octet-stream" })
-
-  var zip = new JSZip();
-  
-  zip.file(originalFileName, new Blob([headerBlob, binaryBlob]))
-  zip.file("backup of " + originalFileName, new Blob([originalFile]))
-
-  zip.generateAsync({ type: "blob" }).then(function(content) {
-    saveAs(content, "save.zip");
-  });
-
-  //saveAs(new Blob([headerBlob, binaryBlob]), "save.xml");
-}
-
-function go(filecontent){
-  var saveJson = LoadSaveJson(filecontent)
+function renderSave(filecontent){
+  var saveJson = LoadSaveJson(filecontent);
+  if (saveJson == null) return;
   document.getElementById('fileInput').innerHTML = ""
   saveEditorElement = new JSONEditor(document.getElementById("saveJson"), {}, saveJson)
+  saveEditorElement.expand({path: ["save"], isExpand: true, recursive: false})
 }
 
 function LoadSaveJson(filecontent)
 {
-  tempsave = filecontent
   parser = new DOMParser();
-  xmlDoc = parser.parseFromString(loadFile(filecontent), "text/xml");
+  var file = loadFile(filecontent)
+  if (file == null) return;
+  document.getElementById("editorParent").style.display = "block"
+  xmlDoc = parser.parseFromString(file, "text/xml");
   return JSON.parse(xml2json(xmlDoc, ""));
-}
-
-function loadFile(data) {
-  const matched = headerRgx.exec(data);
-  saveHeader = matched[0]
-  if (!matched) {
-      console.error('No header match found');
-      return;
-  }
-  const datasizeMatch = datasizeRgx.exec(matched[0]);
-  if (!datasizeMatch) {
-      console.error('No DataSize match found');
-      return;
-  }
-  
-  const saveData = data.slice(matched[0].length, data.length)
-  const decoded = decompress(stringToArrayBuffer(saveData))
-  return dec.decode(decoded);
 }
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,10 @@
+const headerRgx = /^((?:(?:XLZF|[A-Za-z]+:=.*?)\n)+)/;
+const datasizeRgx = /(\nDataSize:=)([0-9]+)\n/;
+const savedatasizeRgx = /(\nSaveDataSize:=)([0-9]+)\n/;
+var enc = new TextEncoder();
+var dec = new TextDecoder();
+var saveHeader;
+
 function stringToArrayBuffer(binaryString) {
     const length = binaryString.length;
     const buffer = new ArrayBuffer(length);
@@ -27,3 +34,43 @@ function stringToArrayBuffer(binaryString) {
         children: [...xmlNode.children].map(childNode => xmlToJson(childNode))
     };
   }
+
+function saveFile(saveJson, originalFile)
+{
+  var saveJson = saveEditorElement.get()
+  var saveXml = json2xml(saveJson)
+  var compressedXml = compress(enc.encode(saveXml))
+  saveHeader = saveHeader.replace(datasizeRgx, "\nDataSize:=" + saveXml.length + "\n")
+  saveHeader = saveHeader.replace(savedatasizeRgx, "\nSaveDataSize:=" + compressedXml.byteLength + "\n")
+  var headerBlob = new Blob([saveHeader], { type: 'text/plain' });
+  var binaryBlob = new Blob([compressedXml], { type: "application/octet-stream" })
+
+  var zip = new JSZip();
+  zip.file(originalFile.name, new Blob([headerBlob, binaryBlob]))
+  zip.file("Backup - " + originalFile.name, new Blob([originalFile]))
+  zip.generateAsync({ type: "blob" }).then(function(content) {
+    saveAs(content, "save.zip");
+  });
+}
+
+function loadFile(data) {
+  const matched = headerRgx.exec(data);
+  saveHeader = matched[0]
+  if (!matched) {
+      console.error('No header match found');
+      return null;
+  }
+  const datasizeMatch = datasizeRgx.exec(matched[0]);
+  if (!datasizeMatch) {
+      console.error('No DataSize match found');
+      return null;
+  }
+  
+  const saveData = data.slice(matched[0].length, data.length)
+  try {
+    const decoded = decompress(stringToArrayBuffer(saveData))
+    return dec.decode(decoded);
+  } catch {
+    return null;
+  }  
+}
